0af768c9c27d931a9b45d60a6cf9e9ddecdbce3c
Philip Nygaard Schmidt <philip.n.schmidt@gmail.com>
2018-09-26 17:00:47 +0200

    Sekvendsdiagram er added

--- Handin2/ATM/ATM/TransponderReceiver.cs
+++ Handin2/ATM/ATM/TransponderReceiver.cs
@@ -7,5 +7,17 @@ using TransponderReceiver; //Needed in order to use the TransponderReceiver dll
 
 namespace ATM
 {
+        public class RawTransponderDataEventArgs : EventArgs
+        {
+            public RawTransponderDataEventArgs(List<string> transponderData)
+            {
+                TransponderData = transponderData;
+            }
+            public List<string> TransponderData { get; }
+        }
+        public interface ITransponderReceiver
+        {
+            event EventHandler<RawTransponderDataEventArgs> TransponderDataReady;
+        }
 
 }
