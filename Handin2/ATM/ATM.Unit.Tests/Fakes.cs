16e346dff418489b43568b9f20a846cb13c72b46
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-09-27 15:08:30 +0200

    Opretter Test-projekt og implementerer FakeLogger og FakeRenderer.

---
+++ Handin2/ATM/ATM.Unit.Tests/Fakes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Unit.Tests
+{
+    class FakeRenderer: IRenderer
+    {
+        public int RenderTrackData_TimesCalled { get; set; }
+        public int RenderSeperationEvent_TimesCalled { get; set; }
+
+        public FakeRenderer()
+        {
+            RenderSeperationEvent_TimesCalled = 0;
+            RenderTrackData_TimesCalled = 0;
+        }
+
+        public void RenderSeperationEvent(SeperationEvent seperationEvent)
+        {
+            RenderSeperationEvent_TimesCalled++;
+        }
+
+        public void RenderTrack(TrackData trackData)
+        {
+            RenderTrackData_TimesCalled++;
+        }
+    }
+
+    class FakeLogger : ILogger
+    {
+        public int LogSeperationEvent_timesCalled { get; set; }
+
+        public FakeLogger()
+        {
+            LogSeperationEvent_timesCalled = 0;
+        }
+
+        public void LogSeperationEvent(SeperationEvent seperationEvent)
+        {
+            LogSeperationEvent_timesCalled++;
+        }
+    }
+}
---
+++ Handin2/ATM/ATM.Unit.Tests/UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Unit.Tests
+{
+    [TestFixture]
+    public class ATM_Unit_Tests
+    {
+        [Test]
+        public void Test_1_is_1()
+        {
+            Assert.That(1.Equals(1));
+        }
+
+
+    }
+}
