27039cfa6a2ab290e1bc4c106df711d0aab702be
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-09-28 09:11:38 +0200

    Implementerer Airspace of påbegynder FakeAirspace

--- Handin2/ATM/ATM/AirSpace.cs
+++ Handin2/ATM/ATM/AirSpace.cs
@@ -9,21 +9,36 @@ namespace ATM
 {
     public class AirSpace : IAirspace
     {
-        private double _Xcord, _Ycord, _Zcord;
-
-        public AirSpace(double Xcord, double Ycord, double Zcord)
+        double _xMin { get; set; }
+        double _xMax { get; set; }
+        double _yMin { get; set; }
+        double _yMax { get; set; }
+        double _zMin { get; set; }
+        double _zMax { get; set; }
+        public Airspace(double xMin, double xMax, double yMin, double yMax, double zMin, double zMax)
         {
-            _Xcord = Xcord;
-            _Ycord = Ycord;
-            _Zcord = Zcord;
+            _xMin = xMin;
+            _xMax = xMax;
+            _yMin = yMin;
+            _yMax = yMax;
+            _zMin = zMin;
+            _zMax = zMax;
         }
 
         public bool CheckIfInMonitoredArea(double xCord, double yCord, double zCord)
         {
-
-            //To be implemented
-
-            return true;
+            if (xCord < _xMax || xCord > _xMin) //Check if xMin<xCord<xMax
+            {
+                if (yCord < _yMax || yCord > _yMin) //Check if yMin<yCord<yMax
+                {
+                    if (zCord < _zMax || zCord > _zMin) //Check if zMin<zCord<zMax
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
+}
 }
