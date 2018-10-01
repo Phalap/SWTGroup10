8ada532c27514d457c2e663b5ae9f66fdab863ae
Dalle <Daniel.toettrup@gmail.com>
2018-10-01 08:28:15 +0200

    Ny implementering af HandleNewTrackData

--- Handin2/ATM/ATM/ATM.cs
+++ Handin2/ATM/ATM/ATM.cs
@@ -31,17 +31,12 @@ namespace ATM
         public void HandleNewTrackData(TrackData trackdata)
         {
 
-            for (int i = 0; i < _currentTracks.Count; i++)
-            {
-                if (_currentTracks[i]._Tag == trackdata._Tag)
-                {
-                    _currentTracks[i]._CurrentXcord = trackdata._CurrentXcord;
-                    _currentTracks[i]._CurrentYcord = trackdata._CurrentYcord;
-                    _currentTracks[i]._CurrentZcord = trackdata._CurrentZcord;
-                    _currentTracks[i]._CurrentCourse = trackdata._CurrentCourse;
-                    _currentTracks[i]._CurrentHorzVel = trackdata._CurrentHorzVel;
-                }
-            }
+            TrackData trackToEdit = _currentTracks.Find(x => x._Tag == trackdata._Tag);
+            trackToEdit._CurrentXcord = trackdata._CurrentXcord;
+            trackToEdit._CurrentYcord = trackdata._CurrentYcord;
+            trackToEdit._CurrentZcord = trackdata._CurrentZcord;
+            trackToEdit._CurrentCourse = trackdata._CurrentCourse;
+            trackToEdit._CurrentHorzVel = trackToEdit._CurrentHorzVel;
 
             if (_currentTracks.Exists(x => x._Tag == trackdata._Tag) == false)
             {
