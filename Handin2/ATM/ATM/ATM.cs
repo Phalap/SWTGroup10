6363d0210490c58b0c9b6c983f787595bbc46ad2
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-10-01 09:36:32 +0200

    Laver Unit Tests for RemoveTrack og AddTrack, samt implementerer logik i RemoveTrack og AddTrack.

--- Handin2/ATM/ATM/ATM.cs
+++ Handin2/ATM/ATM/ATM.cs
@@ -14,8 +14,8 @@ namespace ATM
         private IRenderer _renderer;
         private ITransponderReceiver _transponderReceiver;
 
-        private List<TrackData> _currentTracks;
-        private List<SeperationEvent> _currentSeperationEvents;
+        public List<TrackData> _currentTracks { get; }
+        public List<SeperationEvent> _currentSeperationEvents { get; }
 
         private IAirspace _airspace;
 
@@ -25,7 +25,8 @@ namespace ATM
             _renderer = renderer;
             _transponderReceiver = transponderReceiver;
             _airspace = airspace;
-
+            _currentSeperationEvents = new List<SeperationEvent>();
+            _currentTracks = new List<TrackData>();
         }
 
         public void HandleNewTrackData(TrackData trackdata)
@@ -45,7 +46,7 @@ namespace ATM
 
             if (_currentTracks.Exists(x => x._Tag == trackdata._Tag) == false)
             {
-                AddFlight(trackdata);
+                AddTrack(trackdata);
             }
             
         }
@@ -55,9 +56,28 @@ namespace ATM
             //Denne funktion er ikke nødvendig efter min menning, da det er nemmere at opdatere direkte i for-loopet i HandleNewTrackData.
         }
 
-        public void AddFlight(TrackData trackData)
+        public void AddTrack(TrackData trackData)
         {
-            _currentTracks.Add(trackData);
+            //Check if TrackData with given tag already exists.
+            if(_currentTracks.Exists(x => x._Tag == trackData._Tag))
+            {
+                //Find index of existing data.
+                int index = _currentTracks.FindIndex(x => x._Tag == trackData._Tag);
+                //replace existing data with new data.
+                _currentTracks[index] = trackData;
+            }
+            else
+            {
+                //Add trackData.
+                _currentTracks.Add(trackData);
+            }
+            
+        }
+
+        public void RemoveTrack(string tag)
+        {
+            int index = _currentTracks.FindIndex(x => x._Tag.Equals(tag));
+            _currentTracks.RemoveAt(index);
         }
 
         public bool CheckForSeperationEvent(TrackData trackData1, TrackData trackData2)
