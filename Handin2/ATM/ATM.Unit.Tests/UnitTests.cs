d9915cbbecaf824fe8208d96b8ec71cffea5acdc
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-10-03 10:22:18 +0200

    Påbegynder tests med SubstituteForTransponderReceiver

--- Handin2/ATM/ATM.Unit.Tests/UnitTests.cs
+++ Handin2/ATM/ATM.Unit.Tests/UnitTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using NSubstitute;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -692,6 +693,56 @@ namespace ATM.Unit.Tests
             system.Detach(uut);
 
             Assert.That(() => system.getObserverCount().Equals(0));
+        }
+
+        [Test]
+        public void TransponderReceiver_3NewTracksWithDifferentTags_TrackCountIs3()
+        {
+            //Test inspired by "TransponderReceiverUser" by FRABJ.
+            // Make a fake Transponder Data Receiver
+            var _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            // Inject the fake TDR
+            var transponderReceiver = new TransponderReceiver(_fakeTransponderReceiver);
+
+            // Setup test data
+            List<string> testData = new List<string>();
+            testData.Add("ATR423;39045;12932;14000;20151006213456789");
+            testData.Add("BCD123;10005;85890;12000;20151006213456789");
+            testData.Add("XYZ987;25059;75654;4000;20151006213456789");
+            
+            //Attach uut to receiver
+            transponderReceiver.Attach(uut);
+
+            // Act: Trigger the fake object to execute event invocation
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+
+            Assert.That(uut._currentTracks.Count.Equals(3));
+        }
+
+        [Test]
+        public void TransponderReceiver_3NewTracksWithSameTags_TrackCountIs1()
+        {
+            //Test inspired by "TransponderReceiverUser" by FRABJ.
+            // Make a fake Transponder Data Receiver
+            var _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            // Inject the fake TDR
+            var transponderReceiver = new TransponderReceiver(_fakeTransponderReceiver);
+
+            // Setup test data
+            List<string> testData = new List<string>();
+            testData.Add("ATR423;39045;12932;14000;20151006213456789");
+            testData.Add("ATR423;10005;85890;12000;20151006213456789");
+            testData.Add("ATR423;25059;75654;4000;20151006213456789");
+
+            //Attach uut to receiver
+            transponderReceiver.Attach(uut);
+
+            // Act: Trigger the fake object to execute event invocation
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+
+            Assert.That(uut._currentTracks.Count.Equals(1));
         }
         #endregion
     }
